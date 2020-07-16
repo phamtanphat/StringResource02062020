0466926c36c6ce783050f9fea8ab9d0dd3a16bfa
phatdroid94 <phatdroid94@gmail.com>
2020-07-16 18:44:08 +0700

    setlange local

--- app/src/main/java/com/example/stringresource02062020/MainActivity.java
+++ app/src/main/java/com/example/stringresource02062020/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.stringresource02062020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,12 +21,21 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Resources res = getResources();
+
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+        conf.setLocale(new Locale("en", "US"));
+        res.updateConfiguration(conf, dm);
+
         setContentView(R.layout.activity_main);
 
         mEdtAccount = findViewById(R.id.edittextTaikhoan);
         mEdtPassword = findViewById(R.id.edittextMatkhau);
         mBtnLogin = findViewById(R.id.buttonDangnhap);
         mTvInfo = findViewById(R.id.textviewThongtin);
+
+
 
         mBtnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,7 +50,12 @@ public class MainActivity extends AppCompatActivity {
                         labelTaikhoan + " : " + taikhoan + "\n" +
                         labelMatkhau + " : " + matkhau;
                 mTvInfo.setText(info);
+
+
+
             }
         });
     }
+
+
 }
